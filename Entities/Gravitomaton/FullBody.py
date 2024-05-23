2364d449cd3afd3c0ab52204850cb1c67eba8062
sewakj27 <sewakj27@greenhill.org>
2024-05-23 00:58:27 -0500

    Fixed everything and added gravity changing functionality.

--- Entities/Gravitomaton/FullBody.py
+++ Entities/Gravitomaton/FullBody.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 
 class FullGravitomaton(pygame.sprite.Sprite):
@@ -7,34 +8,186 @@ class FullGravitomaton(pygame.sprite.Sprite):
 
         self.v = v
 
-        self.surf = pygame.image.load(f"{self.v.path}\\Textures\\fullGravitomaton.png").convert()
+        self.surf = pygame.image.load(f"{self.v.path}\\Textures\\fullGravitomaton.png").convert_alpha()
         self.surf = pygame.transform.scale(self.surf, [70, 70])
-        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
         self.rect = self.surf.get_rect()
+        self.rotation = 0  # degrees
 
-        self.rect.move(startPos[0], startPos[1])
-        self.v.windo.blit(self.surf, self.rect)
+        print(self.rect.midtop, self.rect.midleft, self.rect.midright, self.rect.midbottom)
+
+        self.rect.move_ip(startPos[0] - self.rect.width/2, startPos[1] - self.rect.height)
+        self.show()
 
         self.momentum = [0, 0]
+        self.momentumFactor = [10, -20]
 
-    def update(self):
+        self.direction = "bottom"
+        self.directions = {}
+        self._refreshDirections()
 
-        x = self.rect.x + self.momentum[0]
-        y = self.rect.y + self.momentum[1]
+        self.multipliers = {"bottom": [1, 1],
+                           "top": [-1, -1],
+                           "left": [-1, 1],
+                           "right": [1, -1]}
 
-        if x < 0:
-            x = 0
-        if x > self.v.SCREEN_WIDTH:
-            x = self.v.SCREEN_WIDTH
-        if y < 0:
-            y = 0
-        if y > self.v.SCREEN_HEIGHT:
-            y = self.v.SCREEN_HEIGHT
+        self.force = [0, 0]
 
-        self.rect.move(x, y)
+        self.collided = False
+        self.anchored = False
+        self.fall = True
+
+        self.jumping = False
+        self.maxJumps = 2
+        self.jumps = 0
+
+        self.moving = False
+        self.friction = 2
+        self.gravity = 1
+
+    def updatePos(self):
+        self._refreshDirections()
+        self.collided = False
+
+        self.force[0] += self.directions[self.direction][0]
+        self.force[1] += self.directions[self.direction][1]
+
+        x, y = self.force
+
+        if self.rect.x + x < 0:
+            self.rect.move(0, self.rect.y)
+            self.force[0] = 0
+            self.collided = True
+
+            if self.direction == "left":
+                self.jumping = False
+                self.jumps = 0
+
+        if self.rect.x + x > self.v.SCREEN_WIDTH - self.rect.width:
+            self.rect.move(self.v.SCREEN_WIDTH - self.rect.width, self.rect.y)
+            self.force[0] = 0
+            self.collided = True
+
+            if self.direction == "right":
+                self.jumping = False
+                self.jumps = 0
+
+        if self.rect.y + y < 0:
+            self.rect.move(self.rect.x, 0)
+            self.force[1] = 0
+            self.collided = True
+
+            if self.direction == "top":
+                self.jumping = False
+                self.jumps = 0
+
+        if self.rect.y + y > self.v.SCREEN_HEIGHT - self.rect.height:
+            self.rect.move(self.rect.x, self.v.SCREEN_HEIGHT - self.rect.height)
+            self.force[1] = 0
+            self.collided = True
+
+            if self.direction == "bottom":
+                self.jumping = False
+                self.jumps = 0
+
+        # print(self.force, (self.rect.x, self.rect.y), self.collided, self.anchored)
+
+        self.rect.move_ip(self.force[0], self.force[1])
+
+    def basicMovement(self, key):
+        self.momentum = [0, 0]
+
+        if not key[pygame.K_SPACE] and not key[pygame.K_w] and self.jumping and self.jumps < self.maxJumps:
+            self.jumping = False
+
+        if (key[pygame.K_w] or key[pygame.K_SPACE]) and not self.jumping:
+            self.momentum[1] += self.momentumFactor[1]
+            self.jumping = True
+            self.jumps += 1
+
+        if key[pygame.K_a] and not self.jumping and not self.moving:
+            self.momentum[0] = -self.momentumFactor[0]
+            self.moving = True
+        elif key[pygame.K_d] and not self.jumping and not self.moving:
+            self.momentum[0] = self.momentumFactor[0]
+            self.moving = True
+        else:
+            self.moving = False
+
+        if (key[pygame.K_LSHIFT] or key[pygame.K_RSHIFT] or key[pygame.K_s]) and (self.collided or self.anchored):
+            self.force = [0, 0]
+            self.momentum = [0, 0]
+            self.anchored = True
+            self.fall = False
+            self.jumping = False
+            self.jumps = 0
+        else:
+            self.fall = True
+            self.anchored = False
+
+        self._refreshDirections()
+
+    def directionUpdate(self, key):
+
+        if key[pygame.K_DOWN]:
+            self.direction = "bottom"
+            self.rotate(0)
+            print(self.direction)
+        elif key[pygame.K_UP]:
+            self.direction = "top"
+            self.rotate(180)
+            print(self.direction)
+        elif key[pygame.K_LEFT]:
+            self.direction = "left"
+            self.rotate(270)
+            print(self.direction)
+        elif key[pygame.K_RIGHT]:
+            self.direction = "right"
+            self.rotate(90)
+            print(self.direction)
+
+    def gravityUpdate(self):
+
+        if self.force[0] == 0:
+            pass
+        elif not self.jumping and self.force[0] != 0 and self.direction == "bottom":
+            self.momentum[0] -= self.force[0]/self.friction
+        elif not self.jumping and self.force[0] != 0 and self.direction == "top":
+            self.momentum[0] += self.force[0]/self.friction
+
+        if self.force[1] == 0:
+            pass
+        elif not self.jumping and self.force[1] != 0 and self.direction == "right":
+            self.momentum[0] += self.force[1]/self.friction
+        elif not self.jumping and self.force[1] != 0 and self.direction == "left":
+            self.momentum[0] -= self.force[1]/self.friction
+
+        if self.fall and not self.collided and not self.anchored:
+            self.momentum[1] += self.gravity
+
+        self._refreshDirections()
+
+    def run(self):
+        key = pygame.key.get_pressed()
+
+        self.basicMovement(key)
+
+        self.directionUpdate(key)
+
+        self.gravityUpdate()
+
+        self.updatePos()
+
+        self.show()
+
+    def _refreshDirections(self):
+        self.directions = {"bottom": [self.momentum[0], self.momentum[1]],
+                           "top": [-self.momentum[0], -self.momentum[1]],
+                           "left": [-self.momentum[1], self.momentum[0]],
+                           "right": [self.momentum[1], -self.momentum[0]]}
+
+    def rotate(self, a):
+        self.surf = pygame.transform.rotate(self.surf, a - self.rotation)
+        self.rotation = a
 
     def show(self):
         self.v.windo.blit(self.surf, self.rect)
-
-
-
--- testMain.py
+++ testMain.py
@@ -9,11 +9,15 @@ def run(e):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             v.gameLoop.stop()
+        if event.type == pygame.KEYDOWN:
 
-    v.windo.fill((0, 0, 0))
+            if event.key == pygame.K_ESCAPE:
+                v.gameLoop.stop()
+
+    v.windo.fill((255, 255, 255))
 
     for e in v.entities:
-        e.show()
+        e.run()
 
     pygame.display.flip()
 
@@ -22,15 +26,16 @@ pygame.init()
 
 v = Vars()
 v.path = str(pathlib.Path(__file__).parent)
-v.FPS = 100
-v.SCREEN_WIDTH = 600
-v.SCREEN_HEIGHT = 600
+v.FPS = 30
+v.SCREEN_WIDTH = 800
+v.SCREEN_HEIGHT = 800
 v.windo = pygame.display.set_mode([v.SCREEN_WIDTH, v.SCREEN_HEIGHT])
 v.gameLoop = Loop(v, run)
 v.entities = pygame.sprite.Group()
-v.player = FullGravitomaton(v, [300, 300])
+v.player = FullGravitomaton(v, [v.SCREEN_WIDTH/2, v.SCREEN_HEIGHT])
 v.entities.add(v.player)
 
-v.gameLoop.addProcess("update", v.player.update, None)
+# v.gameLoop.addProcess("Update Movement", v.player.updateKeys, None)
+# v.gameLoop.addProcess("Update Gravity", v.player.gravityUpdate, None)
+# v.gameLoop.addProcess("update Pos", v.player.updatePos, None)
 v.gameLoop.run()
-
